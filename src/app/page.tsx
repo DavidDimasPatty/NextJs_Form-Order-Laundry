e397ab283e1f617db4d3aa79a8e945535e82c9bd
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-03-23 22:04:24 +0700

    form + install bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,18 @@ import { Greet } from "./components/greet";
 import { Counter } from "./components/counter";
 export default function Home() {
   return (
-    <h1 className="text-3xl">Welcome</h1>
+    <div className="card card-primary">
+      <div className="card-head">
+        <h1 className="text-3xl">
+          Form Laundry
+        </h1>
+      </div>
+      <div className="card-body">
+
+      </div>
+      <div className="card-footer">
+
+      </div>
+    </div>
   );
 }
