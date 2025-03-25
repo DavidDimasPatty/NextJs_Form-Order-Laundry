45bdc9cc34c2287ac56bac115110fcf2e1e30d65
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-03-25 22:24:22 +0700

    form add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,37 @@ export default function Home() {
           Form Laundry
         </h1>
       </div>
-      <div className="card-body">
-
-      </div>
-      <div className="card-footer">
-
-      </div>
+      <form method="post" action={"/SaveOrder"}>
+        <div className="card-body">
+          <div className="d-flex justify-content-center align-items-center">
+            <label>Nama : </label>
+            <input type="text" name="nama" className="form-control"></input>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <label>Alamat : </label>
+            <input type="text" name="alamat" className="form-control"></input>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <label>No Telepon : </label>
+            <input type="text" name="notelp" className="form-control"></input>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <label>Email : </label>
+            <input type="text" name="email" className="form-control"></input>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <label>Banyak Pakaian : </label>
+            <input type="text" name="banyakP" className="form-control"></input>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <label>Berat Pakaian : </label>
+            <input type="text" name="beratP" className="form-control"></input>
+          </div>
+        </div>
+        <div className="card-footer">
+          <input type="submit" className="btn btn-primary">Simpan</input>
+        </div>
+      </form>
     </div>
   );
 }
